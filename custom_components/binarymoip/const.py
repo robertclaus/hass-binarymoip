cf9d5da6faa499496816bfdeab9b3dec933996d4
Robert Claus <robertclaus@gmail.com>
2026-07-28 16:23:47 -0500

    Update const.py

--- custom_components/binarymoip/const.py
+++ custom_components/binarymoip/const.py
@@ -2,3 +2,13 @@
 
 DOMAIN = "binarymoip"
 DEVICES = "binarymoip_devices"
+
+
+def power_on_key(name):
+    """Options-flow / options-storage key for a receiver's power-on IR code."""
+    return "{}: Power On Code".format(name)
+
+
+def power_off_key(name):
+    """Options-flow / options-storage key for a receiver's power-off IR code."""
+    return "{}: Power Off Code".format(name)
